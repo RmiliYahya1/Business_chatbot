165d368889fed37c1fda89dc58768bdaad2060c0
Rmili yahya <rmiliyahya2@gmail.com>
2025-07-21 16:47:26 +0100

    définition premiere de la tache de l'agent principal

--- business_chatbot/src/business_chatbot/crew.py
+++ business_chatbot/src/business_chatbot/crew.py
@@ -2,36 +2,36 @@ from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
 from typing import List
+import os
+from dotenv import load_dotenv
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
 # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
+load_dotenv()
+MODEL = os.getenv('MODEL')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+if not MODEL:
+    raise ValueError("MODEL environment variable not set. Please check your .env file.")
+if not OPENAI_API_KEY:
+    raise ValueError("OPENAI_API_KEY environment variable not set. Please check your .env file.")
+
 @CrewBase
 class BusinessChatbot():
     """BusinessChatbot crew"""
-
     agents: List[BaseAgent]
     tasks: List[Task]
-
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
     
     # If you would like to add tools to your agents, you can learn more about it here:
     # https://docs.crewai.com/concepts/agents#agent-tools
     @agent
-    def researcher(self) -> Agent:
+    def business_expert(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['business_expert'],
+            llm= MODEL,
             verbose=True
         )
 
-    @agent
-    def reporting_analyst(self) -> Agent:
-        return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
-            verbose=True
-        )
 
     # To learn more about structured task outputs,
     # task dependencies, and task callbacks, check out the documentation:
